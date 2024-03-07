75c29f5c5f5a0040829a05e0c006c098af9dced9
copiousfreetime <jeremy@copiousfreetime.org>
2024-03-06 23:13:06 -0700

    use the local fnv implementation

--- lib/torid/generator.rb
+++ lib/torid/generator.rb
@@ -1,8 +1,8 @@
-require 'fnv'
 require 'socket'
 require 'securerandom'
-require 'torid/clock'
-require 'torid/uuid'
+require_relative './fnv'
+require_relative './clock'
+require_relative './uuid'
 
 module Torid
   # Public: A class that will generate unique identifiers.
@@ -75,7 +75,7 @@ module Torid
     def self.create_node_id( pid = Process.pid )
       hostname = Socket.gethostname
       random   = SecureRandom.hex( 16 )
-      FNV.new.fnv1a_64("#{hostname}-#{pid}-#{random}")
+      Fnv.fnv1a_64("#{hostname}-#{pid}-#{random}")
     end
 
     # Internal: The default generator used by the system.
